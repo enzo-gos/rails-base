93fbfa7729eab64a9b722eff3ad48bd104e2d24f
enzo-gos <enzo.nguyen.gos@gmail.com>
2024-05-29 18:15:55 +0700

    remove simple-lcov

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -92,10 +92,3 @@ RSpec.configure do |config|
   Kernel.srand config.seed
 =end
 end
-
-SimpleCov::Formatter::LcovFormatter.config.report_with_single_file = true
-SimpleCov.formatter = SimpleCov::Formatter::LcovFormatter
-
-SimpleCov.start do
-  add_filter "/spec/"
-end
